1cd275554fa7635fec0544724f86e6c177073a66
Rishendra Ruppa <rishendra1337@gmail.com>
2025-08-17 14:36:20 +0530

    Create Nth_Fibonacci_Term.cpp

---
+++ Week2/Nth_Fibonacci_Term.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int Fibonacci(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return (Fibonacci(n - 1) + Fibonacci(n - 2));
+}
+
+int main(){
+    int n, res;
+    cout<<"Enter a Number: ";
+    cin>>n;
+    res = Fibonacci(n);
+    cout<<res;
+    
+}
