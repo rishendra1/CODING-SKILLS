f97ca6967ac054732685be8a22ffb3394b238616
Rishendra Ruppa <rishendra1337@gmail.com>
2025-08-25 20:43:28 +0530

    Create Summary_Notes_25-08-2025.cpp

---
+++ Week4/Summary_Notes_25-08-2025.cpp
@@ -0,0 +1,15 @@
+/*
+Abstract Approach
+
+Define vector space axiomatically.
+Vectors are objects with two operations:
+
+Concrete Approach
+Defines vectors and matrices as arrays of scalars (numbers).
+
+Matrix : A rectangular array of numbers arranged in:
+
+Rows :- left to right
+Columns :- top to bottom
+Square matrix → Number of rows = Number of columns
+*/
