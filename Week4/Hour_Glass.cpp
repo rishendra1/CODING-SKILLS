900cfc625286f98016ae7e4c39ffd02a580f73c8
Rishendra Ruppa <rishendra1337@gmail.com>
2025-08-25 18:49:54 +0530

    Create Hour_Glass.cpp

---
+++ Week4/Hour_Glass.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int i,j,a[6][6],sum;
+    int max = INT_MIN;
+    for(i = 0 ; i < 6 ; i++){
+        for(j = 0 ; j < 6 ; j++){
+            cout<<"Enter element: ";
+            cin>>a[i][j];
+        }
+    }
+
+    for(i = 0 ; i < 4 ; i++){
+        for(j = 0 ; j < 4 ; j++){
+            sum = a[i][j] + a[i][j + 1] + a[i][j + 2] + a[i + 1][j + 1] + a[i + 2][j] + a[i + 2][j + 1] + a[i + 2][j + 2];
+            if(sum > max){
+                max = sum;
+            }
+        }
+    }
+    cout<<max;
+}
