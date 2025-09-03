9bc5c92d0c2171ba618d83e3208f13e4550a9ed8
Rishendra Ruppa <rishendra1337@gmail.com>
2025-09-03 13:18:29 +0530

    Create pointers.cpp

---
+++ Week5/pointers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+     int a = 20;
+     int *p;
+     p =& a;
+     cout<<*p<<"\n";
+     cout<<a<<"\n";
+     cout<<&a<<"\n";
+     cout<<p<<"\n";
+}
