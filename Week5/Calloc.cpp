2d3ffe741836b7ec5218ba40b9503293e74937c1
Rishendra Ruppa <rishendra1337@gmail.com>
2025-09-03 14:07:20 +0530

    Create Calloc.cpp

---
+++ Week5/Calloc.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    int *a = (int *)calloc(5,sizeof(int));
+    for(int i = 0 ; i < 5 ; i++){
+        cout<<"Enter elements: ";
+        cin>>a[i];
+    }
+    for(int i = 0 ; i < 5 ; i++){
+        cout<<a[i]<<" ";
+    }
+}
