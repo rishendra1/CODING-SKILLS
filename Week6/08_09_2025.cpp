76b422283d767ee032c58fb67f2313f20f1a35cc
Rishendra Ruppa <rishendra1337@gmail.com>
2025-09-09 09:19:20 +0530

    Create 08_09_2025.cpp

---
+++ Week6/08_09_2025.cpp
@@ -0,0 +1,20 @@
+/*
+
+Dynamic Memory Allocation:
+in c,
+1. calloc()
+2. malloc()
+3. realloc()
+4. free()
+
+in c++,
+1. new
+2. delete
+
+Dynamic 2d array:
+    1. Using single pointer
+    2. Using pointer to pointer
+    3. Using array of pointer
+    4. Using pointer to array
+
+*/
