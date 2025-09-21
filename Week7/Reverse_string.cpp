8903b36d2ca4a778b1c1ac0f14811a3208253c7c
Rishendra Ruppa <rishendra1337@gmail.com>
2025-09-21 12:59:08 +0530

    Create Reverse_string.cpp

---
+++ Week7/Reverse_string.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string str,str2;
+    cout<<"Enter Name : ";
+    getline(cin,str);
+    int l = 0 ; 
+    int r = str.length() - 1;
+    while(l < r){
+        char t;
+        t = str[l];
+        str[l] = str[r];
+        str[r] = t;
+        l++;
+        r--;
+    }
+    cout<<str;
+}
