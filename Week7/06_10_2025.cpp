e1c632f2325a2580ca351cb35a9e2691b3e2c1bc
Rishendra Ruppa <rishendra1337@gmail.com>
2025-10-07 09:52:04 +0530

    Create 06_10_2025.cpp

---
+++ Week7/06_10_2025.cpp
@@ -0,0 +1,26 @@
+/*
+
+Functions :- A function is a sub_program ot self-untended block of statement to accomplish a particular task
+Advantages:
+1. code re-usability
+2. code readability
+3. de-bugging is easy
+4. code maintainability
+There are functions. they are:-
+1. Return type without arguments
+2. Return type with arguments
+3. Non-Return type without arguments
+4. Non-Return type with arguments
+
+Storage class :-
+1. Auto - local by default, every variable in c is auto
+    scope : a) block scope
+            b) Function scope
+            c) auto scope
+2. static - will not get deleted until the program ends
+3. register - the variable gest stored in CPU not in RAM
+    ->cannot use pointers
+    ->default value : garbage value
+4. extern : it behaves exactly like a variable in global
+    
+*/
